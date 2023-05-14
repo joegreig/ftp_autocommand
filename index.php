93e954a8cbddd62024cfad8a692f819de31f70ef
joegreig <joegreig1310@hotmail.co.uk>
2023-05-14 10:46:38 +0100

    Added css and error improvement

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="stylesheet.css" />
     <title>FTP AutoCommand</title>
 </head>
 <body>
@@ -11,15 +12,15 @@
     <!-- Input Form -->
     <form action="index.php" method="post">
         <!-- Type -->
-        <label for="type">Type:</label>
         <select name="type" id="type">
+            <option value="" disabled selected hidden>Type</option>
             <option value="ftp">ftp</option>
             <option value="sftp">sftp</option>
         </select><br>
 
         <!-- Command -->
-        <label for="command">Command:</label>
         <select name="command" id="comm">
+            <option value="" disabled selected hidden>Command</option>
             <option value="delete">Delete single file</option>
             <option value="get">Download single file</option>
             <option value="open">Open connection</option>           
@@ -27,17 +28,14 @@
         </select><br>
 
         <!-- Server -->
-        <label for="server">Server:</label>
-        <input typ="text" id="serv" name="server"><br>
+        <input typ="text" id="serv" name="server" placeholder="Server"><br>
 
         <!-- Port -->
-        <label for="port">Port:</label>
-        <input typ="text" id="port" name="port"><br>
+        <input typ="text" id="port" name="port" placeholder="Port"><br>
 
         
         <!-- Username -->
-        <label for="username">Username:</label>
-        <input typ="text" id="uname" name="username"><br>
+        <input typ="text" id="uname" name="username" placeholder="Username"><br>
 
         <!-- Password
         <label for="password">Password:</label>
@@ -45,34 +43,39 @@
         -->
 
         <!-- Local File -->
-        <label for="localfile">Local File:</label>
-        <input typ="text" id="loc_file" name="localfile"><br>
+        <input typ="text" id="loc_file" name="localfile" placeholder="Local File"><br>
 
         <!-- Remote File -->
-        <label for="remotefile">Remote File:</label>
-        <input typ="text" id="rem_file" name="remotefile"><br>
-    <input type="submit"><br><br>
+        <input typ="text" id="rem_file" name="remotefile" placeholder="Remote File"><br>
+    <input type="submit" name="submit"><br><br>
     </form>
 
     <?php
 
-        $final="";
-        $command=$_POST["command"];
-        // echo $command;
+        $final="Command will display here...";
 
-        if ($command=="open"){
-            if ($_POST["type"]=="ftp"){
-                $final="ftp " . $_POST["server"] . " " . $_POST["port"];
-            } elseif($_POST["type"]=="sftp"){
-                $final="sftp -oPort=" . $_POST["port"] . " " . $_POST["username"] . "@" . $_POST["server"];
+        if (isset($_POST["command"]) && isset($_POST["type"])){
+            $command=$_POST["command"];
+            // echo $command;
+
+            if ($command=="open"){
+                if ($_POST["type"]=="ftp"){
+                    $final="ftp " . $_POST["server"] . " " . $_POST["port"];
+                } elseif($_POST["type"]=="sftp"){
+                    $final="sftp -oPort=" . $_POST["port"] . " " . $_POST["username"] . "@" . $_POST["server"];
+                }
+            } elseif ($command=="get"){
+                $final="get " . $_POST["localfile"] . " " . $_POST["remotefile"];
+            } elseif ($command=="put"){
+                $final="put " . $_POST["remotefile"] . " " . $_POST["localfile"];
+            } elseif ($command=="delete"){
+                $final="delete " . $_POST["remotefile"];
             }
-        } elseif ($command=="get"){
-            $final="get " . $_POST["localfile"] . " " . $_POST["remotefile"];
-        } elseif ($command=="put"){
-            $final="put " . $_POST["remotefile"] . " " . $_POST["localfile"];
-        } elseif ($command=="delete"){
-            $final="delete " . $_POST["remotefile"];
+        } elseif (isset($_POST["submit"])){
+            $final="Please select Type and Command dropdowns";
         }
+
+        
 
         //echo $final;
 
